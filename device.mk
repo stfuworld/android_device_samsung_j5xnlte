331d8d12dc3a068f665b74669e8347bc525faf60
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-25 13:58:28 +0100

    j5xnlte: kang nfc configs from j4primelte
    Signed-off-by: Jens Lody <jens@jenslody.de>

--- device.mk
+++ device.mk
@@ -11,3 +11,10 @@ LOCAL_PATH := device/samsung/j5xnlte
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# NFC configs
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
+	$(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/nfc/libnfc-nxp.conf
+
