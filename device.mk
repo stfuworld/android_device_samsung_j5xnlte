9c9ffb3c22790736eb7f3a8e3c7c36ca50882ad4
Ataberk Özen <ataberkozen123@gmail.com>
2020-03-25 14:43:32 +0100

    j5xnlte: build NFC HAL 1.1
    Change-Id: I9ca346fd7c9fece538750facd549e8e87389c20d

--- device.mk
+++ device.mk
@@ -12,6 +12,22 @@ LOCAL_PATH := device/samsung/j5xnlte
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# NFC
+PRODUCT_PACKAGES += \
+	android.hardware.nfc@1.1 \
+	android.hardware.nfc@1.1-service \
+    vendor.nxp.nxpnfc@1.0 \
+    vendor.nxp.hardware.nfc@1.0-service
+
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    com.gsma.services.nfc \
+    com.nxp.nfc.nq \
+    nfc_nci.nqx.default \
+    nqnfcee_access.xml \
+    NfcNci \
+    Tag
+
 # NFC configs
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
