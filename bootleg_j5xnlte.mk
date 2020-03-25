b2c9a9201c12045d50ebf0104e0ebf6f0439a204
Jens Lody <jens@jenslody.de>
2020-03-25 14:07:55 +0100

    j5xnlte: bringup of device-individuation in for Bootleggers
    Signed-off-by: Jens Lody <jens@jenslody.de>

--- bootleg_j5xnlte.mk
+++ bootleg_j5xnlte.mk
@@ -1,15 +1,11 @@
 
 # Inherit from common
-$(call inherit-product, device/samsung/j5x-common/bootleg_j5x-common.mk)
+$(call inherit-product, device/samsung/msm8916-common/bootleg_j5x.mk)
 
 $(call inherit-product, device/samsung/j5xnlte/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5xnlte
 PRODUCT_NAME := bootleg_j5xnlte
-PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J510F
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_CHARACTERISTICS := phone
 
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
