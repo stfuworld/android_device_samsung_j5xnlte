e593c4848868c4a749478e84495b2640d771150b
Jens Lody <jens@jenslody.de>
2020-07-19 21:32:29 +0200

    j5xnlte: Derpify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,8 @@
 LOCAL_PATH := device/samsung/j5xnlte
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_j5xnlte.mk \
-	$(LOCAL_DIR)/rr_j5xnlte.mk \
-	$(LOCAL_DIR)/pa_j5xnlte.mk \
-	$(LOCAL_DIR)/lineage_j5xnlte.mk \
-	$(LOCAL_DIR)/bootleg_j5xnlte.mk
+	$(LOCAL_DIR)/derp_j5xnlte.mk
 	
 COMMON_LUNCH_CHOICES := \
-    bootleg_j5xnlte-eng \
-    bootleg_j5xnlte-userdebug \
-    bootleg_j5xnlte-user
+    derp_j5xnlte-eng \
+    derp_j5xnlte-userdebug \
+    derp_j5xnlte-user
