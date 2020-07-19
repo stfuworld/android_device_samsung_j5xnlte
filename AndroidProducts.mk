cae0fa307f33a9bd714fe867eaaef0251d4692d7
Jens Lody <jens@jenslody.de>
2020-07-19 21:32:29 +0200

    Blissify
    Change-Id: I508a10d19e7f9937474dd9a477e3fbe174cd1b68

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,8 @@
 LOCAL_PATH := device/samsung/j5xnlte
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_j5xnlte.mk \
-	$(LOCAL_DIR)/rr_j5xnlte.mk \
-	$(LOCAL_DIR)/pa_j5xnlte.mk \
-	$(LOCAL_DIR)/lineage_j5xnlte.mk \
-	$(LOCAL_DIR)/bootleg_j5xnlte.mk
+	$(LOCAL_DIR)/bliss_j5xnlte.mk
 	
 COMMON_LUNCH_CHOICES := \
-    bootleg_j5xnlte-eng \
-    bootleg_j5xnlte-userdebug \
-    bootleg_j5xnlte-user
+    bliss_j5xnlte-eng \
+    bliss_j5xnlte-userdebug \
+    bliss_j5xnlte-user
--- bliss_j5xnlte.mk
+++ bliss_j5xnlte.mk
@@ -2,6 +2,6 @@ $(call inherit-product, device/samsung/j5xnlte/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5xnlte
-PRODUCT_NAME := bootleg_j5xnlte
+PRODUCT_NAME := bliss_j5xnlte
 PRODUCT_MODEL := SM-J510F
 
