e10391013a12fbab10f397ae8afd1e6173169f3b
Arbab(AB) <arbabaliabforever@gmail.com>
2021-04-29 21:11:53 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := device/samsung/j5xnlte
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/derp_j5xnlte.mk
+	$(LOCAL_DIR)/aosp_j5xnlte.mk
 	
 COMMON_LUNCH_CHOICES := \
-    derp_j5xnlte-eng \
-    derp_j5xnlte-userdebug \
-    derp_j5xnlte-user
+    aosp_j5xnlte-eng \
+    asop_j5xnlte-userdebug \
+    aosp_j5xnlte-user
